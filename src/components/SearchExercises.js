40745a99bb25c4123cd1d443b227513a05702d41
karthikpratihpati <karthikprathipati1@gmail.com>
2022-06-28 14:58:25 +0530

    added a search bar next up fetching data

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,10 +1,58 @@
-import React,{useEffect,useState} from 'react'
-import { Box,Typography,TextField,Button,Stack } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { Box, Typography, TextField, Button, Stack } from '@mui/material'
 
 
 const SearchExercises = () => {
+  const [search,setSearch] = useState('');
+  function handleSearch(e) {
+    setSearch((prev) => e.target.value)
+  }
   return (
-    <div>SearchExercises</div>
+    <Stack
+      alignItems='center'
+      justifyContent='center'
+      mt='37px'
+      p='20px'
+    >
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: '44px', sm: '30px' } }}
+        mb='50px' textAlign='center'
+      >
+        Awesome Exercises You<br />
+        Should Know
+      </Typography>
+      <Box
+        position='relative' mb='72px'>
+        <TextField
+          type='text' placeholder='Search Exercises' value='' onChange={(e) => { }}
+          sx={{
+            input: {
+              fontWeight: '700',
+              border: 'none',
+              borderRadius: '4px'
+            },
+            width: { lg: '800px', xs: '350px' },
+            backgroundColor: "#fff",
+            borderRadius: '40px'
+          }}
+        />
+        <Button className='search-btn'
+          sx={{
+            bgcolor: '#FF2625',
+            color: '#fff',
+            textTransform: 'none',
+            width: { lg: '175px', xs: '80px' },
+            fontSize: { lg: '20px', sm: '14px' },
+            height: "56px",
+            right: '0'
+
+          }}
+          onClick={handleSearch}
+        >Search</Button>
+
+      </Box>
+    </Stack>
   )
 }
 
