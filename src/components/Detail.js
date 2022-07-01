f4108dd36b9ae82afc74486c1702c64dcad5693a
karthikpratihpati <karthikprathipati1@gmail.com>
2022-07-01 13:00:08 +0530

    created basic exercise detail card and fixed bugs

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Typography, Box, Stack } from '@mui/material';
+import BodyPartImage from '../assets/icons/body-part.png'
+import equipmentImage from '../assets/icons/equipment.png'
+import targetImage from '../assets/icons/target.png'
 
-function Detail() {
-  return (
-    <div>Detail</div>
-  )
+export default function Detail({ exerciseDetail }) {
+
+    const { bodyPart, gifUrl, name, equipment, target } = exerciseDetail
+
+    return (
+        <Stack gap='60px' sx={{ flexDirection: { lg: 'row' }, alignItems: 'center',p:'20px' }} >
+            <img src={gifUrl} alt={name} loading='lazy' className='detail-image' />
+            <Stack sx={{ gap:{lg:'35px' , xs:'20px' }}} >
+                <Typography variant='h3' >
+                    {name}
+                </Typography>
+                <Typography variant='h6' >
+                    Exercises keep you strong.
+                    {` `}{name}{` `} is one of the best exercises to target your {target}.
+                    It'll help improve your mood and keep you in good shape.
+                </Typography>
+            </Stack>
+
+        </Stack>
+    )
 }
 
-export default Detail
--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -4,14 +4,26 @@ import { fetchData, exerciseOptions } from '../utils/fetchData'
 import Detail from '../components/Detail'
 import ExerciseVideos from '../components/ExerciseVideos'
 import SimilarExercises from '../components/SimilarExercises'
+import { Box } from '@mui/material'
 
 const ExerciseDetail = () => {
-  const [ExerciseDetail, setExerciseDetail] = useState({})
-  const {id} = useParams()
+  const [exerciseDetail, setExerciseDetail] = useState({})
+  const { id } = useParams()
+
+  useEffect(() => {
+    async function fetchExerciseDetail() {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com'
+      const youtubeExercise = 'https://youtube-search-and-download.p.rapidapi.com'
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions)
+
+      setExerciseDetail(exerciseDetailData)
+    }
+    fetchExerciseDetail()
+  }, [])
 
   return (
     <Box>
-      <Detail />
+      <Detail exerciseDetail={exerciseDetail} />
       <ExerciseVideos />
       <SimilarExercises />
     </Box>
