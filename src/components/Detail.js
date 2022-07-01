2b26f6a1a5ab40fd016d0d63a5ac41eb57b313bb
karthikpratihpati <karthikprathipati1@gmail.com>
2022-07-01 15:36:03 +0530

    fix child object error in icon stack

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Typography, Box, Stack } from '@mui/material';
+import { Typography, Box, Stack, Button } from '@mui/material';
 import BodyPartImage from '../assets/icons/body-part.png'
 import equipmentImage from '../assets/icons/equipment.png'
 import targetImage from '../assets/icons/target.png'
@@ -8,11 +8,26 @@ export default function Detail({ exerciseDetail }) {
 
     const { bodyPart, gifUrl, name, equipment, target } = exerciseDetail
 
+    const extraDetail = [
+        {
+            icon: BodyPartImage,
+            name:"Eat Sleep"
+        },
+        {
+            icon: targetImage,
+            name: "Exercise"
+        },
+        {
+            icon: equipmentImage,
+            name: "Repeat"
+        }
+    ]
+
     return (
-        <Stack gap='60px' sx={{ flexDirection: { lg: 'row' }, alignItems: 'center',p:'20px' }} >
-            <img src={gifUrl} alt={name} loading='lazy' className='detail-image' />
-            <Stack sx={{ gap:{lg:'35px' , xs:'20px' }}} >
-                <Typography variant='h3' >
+        <Stack gap='60px' sx={{ flexDirection: { lg: 'row' }, alignItems: 'center', p: '20px' }} >
+            <img className='detail-image' src={gifUrl} alt={name} loading='lazy' />
+            <Stack sx={{ gap: { lg: '35px', xs: '20px' } }} >
+                <Typography variant='h4' textTransform='capitalize'>
                     {name}
                 </Typography>
                 <Typography variant='h6' >
@@ -20,8 +35,16 @@ export default function Detail({ exerciseDetail }) {
                     {` `}{name}{` `} is one of the best exercises to target your {target}.
                     It'll help improve your mood and keep you in good shape.
                 </Typography>
+                { extraDetail.map((detail) => (
+                    
+                    <Stack key={detail.name} direction='row' alignItems='center' gap='24px' >
+                        <Button sx={{ background: '#fff2db', borderRadius: '50%', width: '80px', height: '80px' }} >
+                            <img src={detail.icon} alt='bodyPart' style={{ width: '40px', height: '40px' }} />
+                        </Button>
+                        <Typography variant='h6' textTransform='capitalize' color='error'>{detail.name}</Typography>
+                    </Stack>
+                )) }
             </Stack>
-
         </Stack>
     )
 }
