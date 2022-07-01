1bd786f0294b926540b9c5bff0694795a32dd452
karthikpratihpati <karthikprathipati1@gmail.com>
2022-07-01 11:54:12 +0530

    creating new page ExerciseDetail

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Params, useParams } from 'react-router-dom'
+import { fetchData, exerciseOptions } from '../utils/fetchData'
+import Detail from '../components/Detail'
+import ExerciseVideos from '../components/ExerciseVideos'
+import SimilarExercises from '../components/SimilarExercises'
 
 const ExerciseDetail = () => {
+  const [ExerciseDetail, setExerciseDetail] = useState({})
+  const {id} = useParams()
+
   return (
-    <div>ExerciseDetail</div>
+    <Box>
+      <Detail />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
   )
 }
 
