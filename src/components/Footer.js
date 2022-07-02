6f3c94bfad09078737f314a8082862855acee0a9
karthikpratihpati <karthikprathipati1@gmail.com>
2022-07-02 11:33:19 +0530

    added a footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Box,Stack,Typography } from '@mui/material' 
+import Logo from '../assets/images/Logo-1.png'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box mt='80px' bgcolor='#fff3f4'>
+      <Stack gap='40px' alignItems='center' px='40px' pt='24px'>
+      
+      <Typography variant='h6' pb='40px' mt='20px' color='error'>
+        Made with Love by Karthik
+      </Typography>
+      </Stack>
+    </Box>
   )
 }
 
